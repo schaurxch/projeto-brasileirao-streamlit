cd6143c47dd1d8ad856b07e2df98c181e977d007
schaurxch <psm301004@gmail.com>
2025-10-19 17:37:29 -0300

    Commit 19-10-2025

--- src/home.py
+++ src/home.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from utils.db_utils import get_lista_times, get_escudo_path, get_team_id
+from services.service_scoreaxis import mostrar_elenco, mostrar_estatisticas, mostrar_proximos_jogos, mostrar_tabela_atual
 
 def mostrar_home():
     st.title("Brasileirão 2025 :soccer:")
@@ -25,40 +26,19 @@ def mostrar_home():
         st.markdown(f"## {selected_time}")
         team_id = get_team_id(selected_time)
 
-
         if team_id:
             # Elenco
             st.subheader("Informações do Time (Elenco)")
-            st.markdown(f"""
-            <div style="border-width:1px;border-color:rgba(0, 0, 0, 0.15);border-style:solid;border-radius:8px;padding:10px;background:#fff;width:100%">
-                <iframe src="https://www.scoreaxis.com/widget/team-info/{team_id}?autoHeight=0&playersTab=1&matchesTab=0&teamLogo=1&statsTab=0" style="width:100%;height:350px;border:none;transition:all 300ms ease"></iframe>
-            </div>
-            <div style="font-size: 12px; font-family: Arial, sans-serif; text-align: left;">Team data by <a target="_blank" href="https://www.scoreaxis.com/">Scoreaxis</a></div>
-            """, unsafe_allow_html=True)
+            mostrar_elenco(team_id)
 
             # Estatísticas
             st.subheader("Estatísticas da Equipe")
-            st.markdown(f"""
-            <div style="border-width:1px;border-color:rgba(0, 0, 0, 0.15);border-style:solid;border-radius:8px;padding:10px;background:#fff;width:100%">
-                <iframe src="https://www.scoreaxis.com/widget/team-info/{team_id}?autoHeight=0&playersTab=0&matchesTab=0&teamLogo=1&statsTab=1" style="width:100%;height:350px;border:none;transition:all 300ms ease"></iframe>
-            </div>
-            <div style="font-size: 12px; font-family: Arial, sans-serif; text-align: left;">Team data by <a target="_blank" href="https://www.scoreaxis.com/">Scoreaxis</a></div>
-            """, unsafe_allow_html=True)
+            mostrar_estatisticas(team_id)
 
             # Próximos jogos
             st.subheader("Próximas Partidas")
-            st.markdown(f"""
-            <div style="border-width:1px;border-color:rgba(0, 0, 0, 0.15);border-style:solid;border-radius:8px;padding:10px;background:#fff;width:100%">
-                <iframe src="https://www.scoreaxis.com/widget/team-info/{team_id}?autoHeight=0&playersTab=0&matchesTab=1&teamLogo=1&statsTab=0" style="width:100%;height:350px;border:none;transition:all 300ms ease"></iframe>
-            </div>
-            <div style="font-size: 12px; font-family: Arial, sans-serif; text-align: left;">Team data by <a target="_blank" href="https://www.scoreaxis.com/">Scoreaxis</a></div>
-            """, unsafe_allow_html=True)
+            mostrar_proximos_jogos(team_id)
 
         # Tabela Atual
         st.subheader("Tabela Atual")
-        st.markdown("""
-        <div style="border-width:1px;border-color:rgba(0, 0, 0, 0.15);border-style:solid;border-radius:8px;padding:10px;background:#fff;width:100%">
-            <iframe src="https://www.scoreaxis.com/widget/standings-widget/648?autoHeight=0&teamsLogo=1&header=1&links=1" style="width:100%;height:550px;border:none;transition:all 300ms ease"></iframe>
-        </div>
-        <div style="font-size: 12px; font-family: Arial, sans-serif; text-align: left;">Data provided by <a target="_blank" href="https://www.scoreaxis.com/">Scoreaxis</a></div>
-        """, unsafe_allow_html=True)
+        mostrar_tabela_atual()
